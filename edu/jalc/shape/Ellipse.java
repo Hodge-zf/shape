0e838a6acd4fcaf6b06a08be4089d647104984d3
Hodge-zf <xx.skcs@gmail.com>
2017-03-07 22:02:40 -0600

    basic object structure defined

--- edu/jalc/shape/Ellipse.java
+++ edu/jalc/shape/Ellipse.java
@@ -1,5 +1,18 @@
 package edu.jalc.shape;
 
 public class Ellipse extends TwoDimensionalShape{
-  
+
+  private double minorAxis, majorAxis;
+
+  private Ellipse()
+  {
+    this.minorAxis=0;
+    this.majorAxis=0;
+  }
+
+  public Ellipse(double minorAxis, double majorAxis)
+  {
+    this.minorAxis=minorAxis;
+    this.majorAxis=majorAxis;
+  }
 }
