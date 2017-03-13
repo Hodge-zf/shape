47d4f0cef1d54f550cc354c0740601b9ae86b6b0
Hodge-zf <xx.skcs@gmail.com>
2017-03-13 12:20:29 -0500

    methods added to ellipse

--- edu/jalc/shape/Ellipse.java
+++ edu/jalc/shape/Ellipse.java
@@ -1,5 +1,7 @@
 package edu.jalc.shape;
 
+import java.lang.Math*;
+
 public class Ellipse extends TwoDimensionalShape{
 
   private double minorAxis, majorAxis;
@@ -14,5 +16,20 @@ public class Ellipse extends TwoDimensionalShape{
   {
     this.minorAxis=minorAxis;
     this.majorAxis=majorAxis;
+  }
+
+  public double getPerimeter()
+  {
+    return 2*Math.PI*Math.sqrt((Math.Pow(minorAxis,2) + Math.Pow(majorAxis,2))/2);
+  }
+
+  public double getArea()
+  {
+    return Math.PI*minorAxis*majorAxis;
+  }
+
+  public String toString()
+  {
+    return "The ellipse has a perimeter of " + getPerimeter() + "and an area of " + getArea();
   }
 }
